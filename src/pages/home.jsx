cb3d15484f65cdbd32c32b9d45df6cae00f573c3
YvonneKanvu <135970177+YvonneKanvu@users.noreply.github.com>
2023-11-30 14:46:52 +0000

    my first commit

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,32 +3,8 @@ import React from 'react';
 function Home() {
   return (
     <main className="timeline">
-      <h1>Timeline here</h1>
-      <div className = "timeline"></div>
-      <div className="header"></div>      
-      <div className="tweet-editor"></div>      
-      <div className="tweets"></div>      
-      <div className="page-title"></div>      
-      <div className="toptweety"></div>      
-      <div className="avatar"></div>      
-      <div className="tweet-editor-form"></div>      
-      <div className="tweet"></div>      
-      <div className="tweet-avatar"></div>      
-      <div className="tweet-content"></div>      
-      <div className="tweet-body"></div>      
-      <div className="tweet-actions"></div>      
-      <div className="tweet-title"></div>      
-      <div className="tweet-text"></div>      
-      <div className="tweet-image"></div>      
-      <div className="tweet-title-author"></div>      
-      <div className="tweet-title-details"></div>      
-      <div className="tweet-action"></div>      
-      <div className="tweet-editor-input"></div>      
-      <div className="tweet-editor-buttons"></div>      
-      <div className="tweet-editor-actions"></div>      
-      <div className="button"></div>      
-
-
+      <h1>Timeline here</h1> 
+      <div></div><div>
     </main>
   );
 }
