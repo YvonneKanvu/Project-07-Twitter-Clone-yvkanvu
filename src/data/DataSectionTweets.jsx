5254f12a3fd1ceee98fb9e2455077cd81025300c
Yvonne Kanvu <yvkanvu@gmail.com>
2023-12-01 21:17:55 +0100

    add to the data json

---
+++ src/data/DataSectionTweets.jsx
@@ -0,0 +1,31 @@
+const DataSectionTweets = [
+    {
+        tweetIcon : "src\images\Tweet-Profile-Photo.png",
+        tweetTitle : "CNN",
+        tweetAutentic : "src\images\Verified.svg",
+        tweetIdentite : "@CNN",
+        tweetDate : "7m",
+        tweetInfo : "President Joe Biden touted a new agreement reached with the European Union to ease Trump-era tariffs on aluminum and steel as a 'major breakthrough' that would serve to both strengthen the US steel industry and combat the global climate crisis.",
+        tweetImage : "",
+        tweetComment : "57",
+        tweetDiscution : "144",
+        tweetLike : "184",
+
+    },
+    
+    {
+        tweetIcon : "src\images\Tweet-Profile-Photo.png",
+        tweetTitle : "CNN",
+        tweetAutentic : "src\images\Verified.svg",
+        tweetIdentite : "@CNN",
+        tweetDate : "7m",
+        tweetInfo : "President Joe Biden touted a new agreement reached with the European Union to ease Trump-era tariffs on aluminum and steel as a 'major breakthrough' that would serve to both strengthen the US steel industry and combat the global climate crisis.",
+        tweetImage : "",
+        tweetComment : "57",
+        tweetDiscution : "144",
+        tweetLike : "184",
+
+    },
+
+]
+export default DataSectionTweets;
--- src/pages/Tweets.jsx
+++ src/pages/Tweets.jsx
@@ -1,8 +1,36 @@
+import DataSectionTweets from "../data/dataSectionTweets";
 const Tweets = () => {
-    return (  
-        <div className="tweets">
-            
-        </div>
+    return ( 
+        <>
+        {DataSectionTweets.map((tweet, index)=>(
+
+        <div className="tweet" key={index}>
+            <div className="tweet-avatar">
+                <img src={tweet.tweetIcon} alt=""/>
+            </div>
+            <div className="tweet-content">
+                <di className="tweet-body">
+                    <div className="tweet-title">
+                        <span className="tweet-title-author">{tweet.tweetTitle}</span>
+                        <span className="tweet-title-author">{tweet.tweetTitle}</span>
+                        <span className="tweet-title-author">{tweet.tweetTitle}</span>
+                        <span className="tweet-title-author">{tweet.tweetTitle}</span>
+                        <span className="tweet-title-author">{tweet.tweetTitle}</span>
+                    </div>
+                    <div className="tweet-text">
+                    {tweet.tweetInfo}
+                    </div>
+                    <div className="tweet-image">
+                        
+                    </div>
+                </di>
+                <di className="tweet-actions">
+                </di>
+            </div>
+        </div>               
+
+        ))}
+        </> 
     );
 }
  
