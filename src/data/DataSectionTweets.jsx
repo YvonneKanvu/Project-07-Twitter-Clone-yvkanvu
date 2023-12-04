bf4827ca3a4fe6c80d4f3594fac0febf9d923a42
Yvonne Kanvu <yvkanvu@gmail.com>
2023-12-04 14:45:29 +0100

    add a data

--- src/data/DataSectionTweets.jsx
+++ src/data/DataSectionTweets.jsx
@@ -1,6 +1,6 @@
 const DataSectionTweets = [
     {
-        tweetIcon : "src\images\Tweet-Profile-Photo.png",
+        tweetIcon : "src\images\Profile-Photo (1).png",
         tweetTitle : "CNN",
         tweetAutentic : "src\images\Verified.svg",
         tweetIdentite : "@CNN",
@@ -15,17 +15,61 @@ const DataSectionTweets = [
     
     {
         tweetIcon : "src\images\Tweet-Profile-Photo.png",
-        tweetTitle : "CNN",
+        tweetTitle : "The New York Times",
         tweetAutentic : "src\images\Verified.svg",
-        tweetIdentite : "@CNN",
-        tweetDate : "7m",
-        tweetInfo : "President Joe Biden touted a new agreement reached with the European Union to ease Trump-era tariffs on aluminum and steel as a 'major breakthrough' that would serve to both strengthen the US steel industry and combat the global climate crisis.",
-        tweetImage : "",
-        tweetComment : "57",
-        tweetDiscution : "144",
-        tweetLike : "184",
+        tweetIdentite : "@nytimes",
+        tweetDate : "2h",
+        tweetInfo : "Gardening boomed during the pandemic. Six Black writers share how has helped them re-establish,and reimagine, a connection to cultivation and the land.",
+        tweetImage : "src\images\tweet-image.png",
+        tweetComment : "19",
+        tweetDiscution : "48",
+        tweetLike : "482",
 
     },
+
+    {
+        tweetIcon : "src\images\Tweeter.svg",
+        tweetTitle : "Twitter",
+        tweetAutentic : "src\images\Verified.svg",
+        tweetIdentite : "@Twitter",
+        tweetDate : "oct 4",
+        tweetInfo : "BIG NEWS lol jk stillTwitter.",
+        tweetImage : "",
+        tweetComment : "118.7k",
+        tweetDiscution : "785.4k",
+        tweetLike : "3.3k",
+
+    },
+
+    
+    {
+        tweetIcon : "src\images\Tweeter.svg",
+        tweetTitle : "Twitter",
+        tweetAutentic : "src\images\Verified.svg",
+        tweetIdentite : "@Twitter",
+        tweetDate : "oct 4",
+        tweetInfo : "hello literally everyone.",
+        tweetImage : "",
+        tweetComment : "118.7k",
+        tweetDiscution : "785.4k",
+        tweetLike : "3.3k",
+
+    },
+
+    {
+        tweetIcon : "src\images\Tweeter.svg",
+        tweetTitle : "Twitter",
+        tweetAutentic : "src\images\Verified.svg",
+        tweetIdentite : "@Twitter",
+        tweetDate : "oct 4",
+        tweetInfo : "hello literally everyone.",
+        tweetImage : "",
+        tweetComment : "",
+        tweetDiscution : "",
+        tweetLike : "",
+
+    },
+    
 
 ]
 export default DataSectionTweets;
