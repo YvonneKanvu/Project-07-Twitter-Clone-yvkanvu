2b3b94b4200fd1533392023f265a8dc5b210296a
Yvonne Kanvu <yvkanvu@gmail.com>
2023-12-31 23:12:34 +0100

    creation de route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { BrowserRouter,Routes,Route,NavLink } from "react-router-dom";
 import Home from "./pages/home.jsx";
 import Layout from "./components/layout.jsx";
 import Sidebar from "./pages/Sidebar.jsx";
 import Trends from "./pages/Trends.jsx";
+import HomeTweet from "./components/HomeTweet.jsx";
 import('./style/reset.css');
 import('./style/App.css');
 
@@ -11,6 +13,15 @@ export default function App() {
       <Sidebar/>
       <Home />
       <Trends/>
+      <BrowserRouter>
+      <NavBar/>
+      <Routes>
+        <Route>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/HomeTweet" element={<HomeTweet/>}/>
+        </Route>
+      </Routes>
+      </BrowserRouter>
     </Layout>
   );
 }
