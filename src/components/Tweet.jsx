7c3a3d80c3126208078b13ba9bf7ca292f0d7fa3
Yvonne Kanvu <yvkanvu@gmail.com>
2024-01-18 16:28:56 +0100

    hover image et chiffre

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,72 +1,90 @@
 import { useState } from "react";
-import { Icon } from '@iconify/react';
+import { Icon } from "@iconify/react";
 
-function Tweet({tweet}) {
-    const [colors, setColors] = useState('',0);
-    const [colors2, setColors2] = useState('');
-    const [colors3, setColors3] = useState('');
-    const handleClic1 = () => { setColors('#0000ff') }
-    const handleClic2 = () => { setColors2('#00ff00') }
-    const handleClic3 = () => { setColors3('#ff0000') }
-    const [comment, setComment] = useState(0)
+export default function Tweet({ tweet }) {
+  const [colors, setColors] = useState("", 0);
+  const [colors2, setColors2] = useState("");
+  const [colors3, setColors3] = useState("");
+  const handleClic1 = () => {
+    setColors("#0000ff");
+  };
+  const handleClic2 = () => {
+    setColors2("#00ff00");
+  };
+  const handleClic3 = () => {
+    setColors3("#ff0000");
+  };
+  const [comment, setComment] = useState(0);
 
-    const handleClick = ()=>{
-    if (comment === 0){
-    setComment ( comment + 1)
-    }else{
-      setComment(comment - 1)
-    };
+  const handleClick = () => {
+    if (comment === 0) {
+      setComment(comment + 1);
+    } else {
+      setComment(comment - 1);
+    }
     const handleClic1 = () => {
-        handleClick();
-        setColors("#0000ff");
-      };
-    
-}
+      handleClick();
+      setColors("#0000ff");
+    };
+  };
 
-    return <div className="tweet">
+  return (
+    <div className="tweet">
+      <div className="tweet-avatar">
+        <img src={tweet.tweetIcon} alt="" />
+      </div>
+      <div className="tweet-content">
+        <div className="tweet-body">
+          <div className="tweet-title">
+            <span className="tweet-title-author">{tweet.tweetTitle}</span>
+            <span className="tweet-title-details">
+              <img src={tweet.tweetAutentic} alt="" />
+            </span>
+            <span className="tweet-title-details">{tweet.tweetIdentite}</span>
+            <span className="tweet-title-details">{tweet.tweetPoint}</span>
+            <span className="tweet-title-details">{tweet.tweetDate}</span>
+          </div>
+          <div className="tweet-text">{tweet.tweetInfo}</div>
+          <div className="tweet-image">
+            <img src={tweet.tweetImage} alt="" />
+          </div>
+        </div>
 
-        <div className="tweet-avatar">
-            <img src={tweet.tweetIcon} alt="" />
+        <div className="tweet-actions">
+          <div
+            className="tweet-action commentaire "
+            onClick={handleClic1}
+            style={{ color: colors }}
+          >
+            <span className="coment">
+              <Icon icon="basil:comment-solid" style={{ color: colors }} />
+            </span>
+            {/* <img src="src/images/Reply.svg" alt="" /> */}
+            <span>{tweet.tweetComment}</span>
+          </div>
+          <div
+            className="tweet-action discution"
+            onClick={handleClic2}
+            style={{ color: colors2 }}
+          >
+            <Icon icon="ei:retweet" style={{ color: colors2 }} />
+            {/* <img src="src\images\Retweet.svg" alt="" /> */}
+            {tweet.tweetDiscution} 
+          </div>
+          <div
+            className="tweet-action aime"
+            onClick={handleClic3}
+            style={{ color: colors3 }}
+          >
+            <Icon icon="icon-park-solid:like" style={{ color: colors3 }} />
+            {/* <img src="src\images\React.svg" alt="" /> */}
+            {tweet.tweetLike}
+          </div>
+          <div className="tweet-action">
+            <img src="src\images\Tweet-Replies.svg" alt="" />
+          </div>
         </div>
-        <div className="tweet-content">
-            <div className="tweet-body">
-                <div className="tweet-title">
-                    <span className="tweet-title-author">{tweet.tweetTitle}</span>
-                    <span className="tweet-title-details"><img src={tweet.tweetAutentic} alt="" /></span>
-                    <span className="tweet-title-details">{tweet.tweetIdentite}</span>
-                    <span className="tweet-title-details">{tweet.tweetPoint}</span>
-                    <span className="tweet-title-details">{tweet.tweetDate}</span>
-                </div>
-                <div className="tweet-text">
-                    {tweet.tweetInfo}
-                </div>
-                <div className="tweet-image">
-                    <img src={tweet.tweetImage} alt="" />
-                </div>
-            </div>
-        
-            <div className="tweet-actions">
-                    <div className="tweet-action commentaire " onClick={handleClic1}   style={{ color: colors }}>
-                    <Icon icon="basil:comment-solid" style={{ color: colors }}/>
-                        {/* <img src="src/images/Reply.svg" alt="" /> */}
-                        {tweet.tweetComment}
-                    </div>
-                <div className="tweet-action discution" onClick={handleClic2} style={{ color: colors2 }}>
-                <Icon icon="ei:retweet" style={{ color: colors2 }} />
-                    {/* <img src="src\images\Retweet.svg" alt="" /> */}
-                    {tweet.tweetDiscution}
-                </div>
-                <div className="tweet-action aime" onClick={handleClic3} style={{ color: colors3 }}>
-                <Icon icon="icon-park-solid:like" style={{ color: colors3 }} />
-                    {/* <img src="src\images\React.svg" alt="" /> */}
-                    {tweet.tweetLike}
-                </div>
-                <div className="tweet-action">
-                    <img src="src\images\Tweet-Replies.svg" alt="" />
-                </div>
-            </div> 
-        </div>
+      </div>
     </div>
+  );
 }
-
-export default Tweet;
