794ae8e3574950cf2c30281db12fd60ef25b301e
Yvonne Kanvu <yvkanvu@gmail.com>
2024-01-25 01:02:21 +0100

    derniere modification sur l'affichage de la page profile

--- src/pages/PageProfil.jsx
+++ src/pages/PageProfil.jsx
@@ -1,12 +1,13 @@
 import { Icon } from "@iconify/react";
 
-
 function PageProfil() {
     
         return (
              <div className="timeline">
-                <h1 className="utilisateurProfil">Yvonne Kanvu</h1>
-                <Icon className="fleshReturn" icon="ph:arrow-left-bold" />
+              <div  className="utilisateurProfil">
+                 <Icon className="fleshReturn" icon="ph:arrow-left-bold" />
+                 <h1>Yvonne Kanvu</h1>
+              </div>
                 <div className="baniere"> 
                   <img  src="src\images\Bitmap.png"className="utilisateurImage" alt="" />
                 </div>
